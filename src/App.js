315627555699214708a196c103eb98a4ef60be3e
allyelvis <allyelvis6569@gmail.com>
2024-12-29 18:58:21 +0000

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Aenzbi Suite</h1>
+      <p>Business and Entertainment Dashboard</p>
+    </div>
+  );
+}
+
+export default App;
